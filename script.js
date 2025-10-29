4124da30b606cacd076ef86c69cbe50f91241fa6
Youness Ait Jamaa <aitjamaayouness1@gmail.com>
2025-10-29 15:29:03 +0100

    Add the values to inputs as text

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const typeReservation = document.getElementById('typeReservation');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+
     const name =  document.getElementById('nomClient').value;
     const day = document.getElementById('jour').value;
     const heurDebut = document.getElementById('heureDebut').value;
@@ -20,6 +21,15 @@ form.addEventListener('submit', (e) => {
     const div = document.createElement('div');
     const paragraph = document.createElement('p');
     const header = document.createElement('h4');
+
+    header.textContent = `Réservation ${typeReservation.value.toUpperCase()} pour : ${name}`;
+    paragraph.textContent = `Début à : ${heurDebut}`;
+
+    div.appendChild(header);
+    div.appendChild(paragraph);
+
+    header.ondblclick = () => makeEditable(header);
+    paragraph.ondblclick = () => makeEditable(paragraph);
 });
 
 
