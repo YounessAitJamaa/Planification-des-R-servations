e0b743df49b422df994f773f4ed7c8819bbd5a3d
Youness Ait Jamaa <aitjamaayouness1@gmail.com>
2025-10-29 15:13:20 +0100

    Starting work on javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+// Copying the days div to javasctipt
+
+const Lundi = document.getElementById('Lundi');
+const Mardi = document.getElementById('Mardi');
+const Mercredi = document.getElementById('Mercredi');
+const Jeudi = document.getElementById('Jeudi');
+const Vendredi = document.getElementById('Vendredi');
+
+
